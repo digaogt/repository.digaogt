420ca8b4795d76a8daf4b6252993ebf31e3c0ec8
Rodrigo Torres <digaogt@gmail.com>
2018-08-07 01:53:53 -0700

    Atualização do run.sh

--- run1.sh
+++
@@ -1,6 +0,0 @@
-git push
-./create_repository.py https://github.com/olavopeixoto/plugin.video.brplay https://github.com/jdf76/plugin.video.youtube ../plugin.video.spotifytube  ../plugin.video.live.streamspro ../script.module.liveresolver-0.1.50.zip ../script.video.F4mProxy-2.8.8.zip ../plugin.video.f4mTester-2.7.2.zip
-sleep 7
-git add *
-git commit -a -m "Atualização de repositório na data de: `date`"
-git push
