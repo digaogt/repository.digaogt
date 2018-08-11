4128a2b7467f9ee8b7d1ea0f9f7bab03c1b705f7
Rodrigo Torres <digaogt@gmail.com>
2018-08-11 15:04:04 -0700

    Atualização de repositório na data de: Sat Aug 11 15:04:04 PDT 2018

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 git pull
-./create_repository.py https://github.com/olavopeixoto/plugin.video.brplay https://github.com/jdf76/plugin.video.youtube ../plugin.video.spotifytube  ../plugin.video.live.streamspro ../script.module.liveresolver ../script.video.F4mProxy-2.8.8 ../plugin.video.f4mTester-2.7.2
+./create_repository.py https://github.com/olavopeixoto/plugin.video.brplay https://github.com/jdf76/plugin.video.youtube ../plugin.video.spotifytube  ../plugin.video.live.streamspro ../script.module.liveresolver ../script.video.F4mProxy ../plugin.video.f4mTester
 sleep 7
 git add *
 git commit -a -m "Atualização de repositório na data de: `date`"
