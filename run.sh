77205ade5afd60775bc829d4facac0aea86bc8b8
Rodrigo Goncalves <digaogt@gmail.com>
2018-10-28 19:47:59 -0700

    Atualização de repositório na data de: Sun Oct 28 19:47:59 PDT 2018

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 git pull
 ./create_repository.py https://github.com/olavopeixoto/plugin.video.brplay https://github.com/jdf76/plugin.video.youtube  ../plugin.video.live.streamspro ../script.module.liveresolver ../script.video.F4mProxy ../plugin.video.f4mTester ../script.brguide
 sleep 7
