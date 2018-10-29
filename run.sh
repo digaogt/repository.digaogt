3453c1cabaf7ef231b04ce106d827da55256dafe
Rodrigo Goncalves <digaogt@gmail.com>
2018-10-29 16:19:29 -0700

    Atualização de repositório na data de: Mon Oct 29 16:19:29 PDT 2018

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd /Users/rodrigogoncalves/Projetos/Git/repository.digaogt
 git pull
-./create_repository.py https://github.com/olavopeixoto/plugin.video.brplay https://github.com/jdf76/plugin.video.youtube  ../plugin.video.live.streamspro ../script.module.liveresolver ../script.video.F4mProxy ../plugin.video.f4mTester ../script.brguide ../skin.fusex
+./create_repository.py https://github.com/olavopeixoto/plugin.video.brplay https://github.com/jdf76/plugin.video.youtube  ../plugin.video.live.streamspro ../script.module.liveresolver ../script.video.F4mProxy ../plugin.video.f4mTester ../script.brguide ../skin.fusex ../spotitube
 sleep 7
 git add *
 git commit -a -m "Atualização de repositório na data de: `date`"
