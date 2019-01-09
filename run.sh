cdca6c53003b82932cace520a89e0ee0d4aa2fb1
Rodrigo Goncalves <digaogt@gmail.com>
2019-01-09 07:51:18 -0800

    Atualização de repositório na data de: Wed Jan  9 07:51:18 PST 2019

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd /Users/rodrigogoncalves/Projetos/Git/repository.digaogt
 git pull
-./create_repository.py https://github.com/olavopeixoto/plugin.video.brplay https://github.com/jdf76/plugin.video.youtube  ../plugin.video.live.streamspro ../script.module.liveresolver ../script.video.F4mProxy ../plugin.video.f4mTester ../script.brguide ../skin.fusex ../plugin.video.spotitube ../script.ivueguide ../script.module.hyper
+./create_repository.py https://github.com/olavopeixoto/plugin.video.brplay https://github.com/jdf76/plugin.video.youtube  ../plugin.video.live.streamspro ../script.module.liveresolver ../script.video.F4mProxy ../plugin.video.f4mTester ../script.brguide https://github.com/Collabsvito/Repo/tree/master/plugin.video.spotitube ../script.ivueguide
 sleep 7
 git add *
 git commit -a -m "Atualização de repositório na data de: `date`"
